f6de7078f3bef9c7bc3214635a7c9dcfb8329f67
R3Dki <106279482+R3Dki@users.noreply.github.com>
2023-01-29 21:09:54 +0100

    Features + Optimization Update
    Remade the Line Drawer(but if you want you can keep using the old one).
    Added Options to Enable Nodes or Experimental Line Drawer.

--- Node Generator.js
+++ Node Generator.js
@@ -1,45 +1,47 @@
 //Config
 const elementId = 'background'; //ID of your Canvas Element
-const nNodes = Math.floor(window.innerHeight/240)*10; //Number of Nodes to create, in this case i create 10 nodes every 240px of resolution.
+const nNodes = Math.floor(window.innerHeight/240)*15; //Number of Nodes to create, in this case i create 10 nodes every 240px of resolution.
 const minAcceleration = -1.8; //Max Negative Acceleration
 const baseAcceleration = 1.0; //Min Acceleration(+/-)
 const maxAcceleration = 1.6; //Max Positive Acceleration
 //Graphic Customization
 const showWatermark = true;
-let nodeLineDist = 30; //Distance between Nodes to Draw a Line
+const nodeLineDist = 150; //Distance between Nodes to Draw a Line
 const lineWidth = 1; //Width of Lines that connect nodes (laggy if over 1 for some particular reason)
 const lineColor = 'green'; //Color of the Lines
+const enableNodes = true;
 const nodeColor = '#BF0000'; //Color of the Nodes (Circles)
 const nodeAlpha = 1; //Opacity of the Nodes
-const opacityDivisionFactor = 500; //It is used to modify the opacity based on node distance
+const opacityDivisionFactor = 100; //It is used to modify the opacity based on node distance
 const minRad = 3 //Min Random Radius of Nodes, also if you want the same node radius just set both minRad and maxRad to the same value
-const maxRad = 7 //Max Random Radius of Nodes
+const maxRad = 5 //Max Random Radius of Nodes
+//Experimental Features
+const UseExperimentalLineDrawer = true;
+// /\ If true it enables the experimental Line Drawer that uses the distance between 2 points formula(Could Lag)(More precise, circular area)
+// /\ If false it enables the default Line Drawer that's made to be lightweight and fast(Less precise, square area)
+const randomColorLinesMode = false; //Select wheter Lines have Random Color or not
 const randomColorList = ['red', 'blue', 'green', 'yellow', 'cyan', 'purple', 'pink', 'white', 'lime', 'aqua', 'silver', 'gray', 'brown'];
 // /\ The list of colors that the Random Color functions can use, you may add or remove some colors /\
-const randomColorLinesMode = false; //Select wheter Lines have Random Color or not
 const randomColorLinesIterations = 200; //After how many Iterations should the Color change
 const randomColorNodesMode = true; //Select wheter Nodes have Random Color or not
 const randomColorNodesIterations = 200; //After how many Iterations should the Color change
+const pulsatingNodeSize = false; //Makes Nodes' radius bigger and smaller depending on how you set Min/Max Pulse Size.
+let minPulseSize = 1; //Minimum Nodes' radius decrease
+let maxPulseSize = 5; //Maximum Nodes' radius increase
 
 //Code
-let prevColorLines;
-let randomCountLines = randomColorLinesIterations+1;
-let prevColorNodes;
-let randomCountNodes = randomColorNodesIterations+1;
+let prevColorLines, randomCountLines = randomColorLinesIterations+1;
+let prevColorNodes, randomCountNodes = randomColorNodesIterations+1;
+let distX, distY, min = 0, nodeCount = 1, maxY = window.innerHeight, maxX = window.innerWidth;
+let nodeList = [];
 const canvas = document.getElementById(elementId);
 const ctx = canvas.getContext('2d');
-ctx.lineWidth = lineWidth;
-let nodeList = [];
-let distX, distY, min = 0;
-let nodeCount = 0;
-nodeLineDist *= 10;
-let maxY = window.innerHeight;
-let maxX = window.innerWidth;
 Begin();
 Renderer();
 
 //Functions
 async function RandomMode(params) {
+    ctx.lineWidth = lineWidth;
     if (randomColorLinesMode) {  
         randomCountLines++;
         if (randomCountLines > randomColorLinesIterations) {
@@ -49,7 +51,7 @@ async function RandomMode(params) {
         }
         ctx.strokeStyle =  prevColorLines;
     } else {ctx.strokeStyle =  lineColor}
-
+    
     if (randomColorNodesMode) {  
         randomCountNodes++;
         if (randomCountNodes > randomColorNodesIterations) {
@@ -59,6 +61,20 @@ async function RandomMode(params) {
         }
         ctx.fillStyle =  prevColorNodes;
     } else {ctx.fillStyle =  nodeColor}
+}
+
+async function PulseNodes(i) {
+    if (pulsatingNodeSize) {   
+        if (nodeList[i].radius >= maxPulseSize) {
+            nodeList[i].pulsating = !nodeList[i].pulsating;
+        }
+        if (nodeList[i].radius <= minPulseSize) {
+            nodeList[i].pulsating = !nodeList[i].pulsating;
+        }
+        if(nodeList[i].pulsating) {nodeList[i].radius++;}
+        else
+        {nodeList[i].radius--;}
+    }
 }
 
 async function Watermark() {
@@ -78,39 +94,58 @@ function Node() {
     this.accelerationY = Math.floor(Math.random() * (maxAcceleration - minAcceleration + 1) + minAcceleration);
     if (this.accelerationX < baseAcceleration && this.accelerationX > -baseAcceleration) {this.accelerationX = baseAcceleration}
     if (this.accelerationY < baseAcceleration && this.accelerationY > -baseAcceleration) {this.accelerationY = baseAcceleration}
-    this.radius = Math.floor(Math.random() * (maxRad - minRad + 1) + minRad);
     this.radius = minRad;
     if (minRad != maxRad) {
         this.radius = Math.floor(Math.random() * (maxRad - minRad + 1) + minRad);
     }
+    if(pulsatingNodeSize){
+        this.radius = minPulseSize+1;
+        this.pulsating = true;
+    }
 }
 
 function Begin() {
+    //Vars Check
+    minPulseSize = Math.abs(minPulseSize);
+    maxPulseSize = Math.abs(maxPulseSize);
     while (nNodes != nodeList.length) {
         nodeList.push(new Node());
         nodeCount = nodeList.length;
     }
 }
 
-//Experimental
-async function LineDrawAsync(i) {
-    for (let i2 = i; i2 < nodeCount; i2++) {
-        distX = nodeList[i2].x-nodeList[i].x;
-        distY = nodeList[i2].y-nodeList[i].y;
-        if ((distX < nodeLineDist && distY < nodeLineDist && distX > -nodeLineDist && distY > -nodeLineDist)) {
-            ctx.moveTo(nodeList[i2].x, nodeList[i2].y);
-            ctx.lineTo(nodeList[i].x, nodeList[i].y);
-            ctx.globalAlpha = (distX+distY)/opacityDivisionFactor;
-            ctx.stroke();
+async function LineDrawAsyncExperimental(i) {
+    if (UseExperimentalLineDrawer) {
+        for (let j = i; j < nodeCount; j++) {
+            distance = Math.sqrt(Math.pow((nodeList[j].x-nodeList[i].x),2)+Math.pow((nodeList[j].y-nodeList[i].y),2));
+            if (Math.abs(distance) <= nodeLineDist) {
+                ctx.moveTo(nodeList[i].x, nodeList[i].y);
+                ctx.lineTo(nodeList[j].x, nodeList[j].y);
+                ctx.globalAlpha = opacityDivisionFactor / distance;
+                ctx.stroke();
+            }
+        }
+    }else{
+        for (let j = i; j < nodeCount; j++) {
+            distX = nodeList[j].x-nodeList[i].x;
+            distY = nodeList[j].y-nodeList[i].y;
+            if (Math.abs(distX) <= nodeLineDist && Math.abs(distY) <= nodeLineDist) {
+                ctx.moveTo(nodeList[j].x, nodeList[j].y);
+                ctx.lineTo(nodeList[i].x, nodeList[i].y);
+                ctx.globalAlpha = opacityDivisionFactor/(Math.abs(distX)+Math.abs(distY));
+                ctx.stroke();
+            }
         }
     }
 }
 
 async function NodeDrawAsync(i) {
-    ctx.globalAlpha=nodeAlpha;
+    if (enableNodes) {
+        ctx.globalAlpha=nodeAlpha;
         ctx.moveTo(nodeList[i].x, nodeList[i].y);
         ctx.arc(nodeList[i].x, nodeList[i].y, nodeList[i].radius, 0, 6.28318531);
         ctx.fill();
+    }
 }
 //Experimental
 
@@ -133,8 +168,9 @@ function Renderer() {
         nodeList[i].y += nodeList[i].accelerationY;
         //Draw Lines between nearing Nodes
         ctx.beginPath();
-        LineDrawAsync(i);
+        LineDrawAsyncExperimental(i);
         //Draw Circles were nodes are
+        PulseNodes(i);
         NodeDrawAsync(i);
     }
     requestAnimationFrame(Renderer);
