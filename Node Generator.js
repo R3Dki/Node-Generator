202e2c4157351c22ebfa9e4386c1c30e49d59d3d
R3Dki <106279482+R3Dki@users.noreply.github.com>
2023-01-29 23:15:39 +0100

    Features Update
    +Added Drawer Enable option
    +Added Node Enable option
    +Added Polygon Drawer

--- Node Generator.js
+++ Node Generator.js
@@ -1,33 +1,37 @@
-//Config
+//============Code Config============//
 const elementId = 'background'; //ID of your Canvas Element
 const nNodes = Math.floor(window.innerHeight/240)*15; //Number of Nodes to create, in this case i create 10 nodes every 240px of resolution.
 const minAcceleration = -1.8; //Max Negative Acceleration
 const baseAcceleration = 1.0; //Min Acceleration(+/-)
 const maxAcceleration = 1.6; //Max Positive Acceleration
-//Graphic Customization
+//============Graphic Customization============//
 const showWatermark = true;
-const nodeLineDist = 150; //Distance between Nodes to Draw a Line
+const nodeLineDist = 200; //Distance between Nodes to Draw a Line
 const lineWidth = 1; //Width of Lines that connect nodes (laggy if over 1 for some particular reason)
 const lineColor = 'green'; //Color of the Lines
-const enableNodes = true;
+const enableNodes = true; //Enables Nodes (just Graphically since they are needed)
 const nodeColor = '#BF0000'; //Color of the Nodes (Circles)
 const nodeAlpha = 1; //Opacity of the Nodes
 const opacityDivisionFactor = 100; //It is used to modify the opacity based on node distance
 const minRad = 3 //Min Random Radius of Nodes, also if you want the same node radius just set both minRad and maxRad to the same value
-const maxRad = 5 //Max Random Radius of Nodes
-//Experimental Features
-const UseExperimentalLineDrawer = true;
+const maxRad = 6 //Max Random Radius of Nodes
+//============Experimental Features============//
+const enableDrawer = true; // Enables Drawers, wich draw lines or polygons(depending on how you set it)
+const useExperimentalLineDrawer = true;
 // /\ If true it enables the experimental Line Drawer that uses the distance between 2 points formula(Could Lag)(More precise, circular area)
 // /\ If false it enables the default Line Drawer that's made to be lightweight and fast(Less precise, square area)
-const randomColorLinesMode = false; //Select wheter Lines have Random Color or not
+const polygonDrawer = false; //Enables Polygon Drawer wich makes every at least 3 Nodes' gap filled
 const randomColorList = ['red', 'blue', 'green', 'yellow', 'cyan', 'purple', 'pink', 'white', 'lime', 'aqua', 'silver', 'gray', 'brown'];
 // /\ The list of colors that the Random Color functions can use, you may add or remove some colors /\
+const randomColorLinesMode = false; //Select wheter Lines have Random Color or not
 const randomColorLinesIterations = 200; //After how many Iterations should the Color change
 const randomColorNodesMode = true; //Select wheter Nodes have Random Color or not
 const randomColorNodesIterations = 200; //After how many Iterations should the Color change
 const pulsatingNodeSize = false; //Makes Nodes' radius bigger and smaller depending on how you set Min/Max Pulse Size.
 let minPulseSize = 1; //Minimum Nodes' radius decrease
 let maxPulseSize = 5; //Maximum Nodes' radius increase
+//============End of Config============//
+
 
 //Code
 let prevColorLines, randomCountLines = randomColorLinesIterations+1;
@@ -114,8 +118,8 @@ function Begin() {
     }
 }
 
-async function LineDrawAsyncExperimental(i) {
-    if (UseExperimentalLineDrawer) {
+async function DrawerAsyncExperimental(i) {
+    if (useExperimentalLineDrawer && !polygonDrawer) {
         for (let j = i; j < nodeCount; j++) {
             distance = Math.sqrt(Math.pow((nodeList[j].x-nodeList[i].x),2)+Math.pow((nodeList[j].y-nodeList[i].y),2));
             if (Math.abs(distance) <= nodeLineDist) {
@@ -125,7 +129,7 @@ async function LineDrawAsyncExperimental(i) {
                 ctx.stroke();
             }
         }
-    }else{
+    }else if (!polygonDrawer) {
         for (let j = i; j < nodeCount; j++) {
             distX = nodeList[j].x-nodeList[i].x;
             distY = nodeList[j].y-nodeList[i].y;
@@ -136,6 +140,14 @@ async function LineDrawAsyncExperimental(i) {
                 ctx.stroke();
             }
         }
+    }else{
+        for (let j = i; j < nodeCount; j++) {
+            distance = Math.sqrt(Math.pow((nodeList[j].x-nodeList[i].x),2)+Math.pow((nodeList[j].y-nodeList[i].y),2));
+            ctx.globalAlpha = 1;
+            if (Math.abs(distance) <= nodeLineDist) {
+                ctx.lineTo(nodeList[j].x, nodeList[j].y);
+            }
+        }
     }
 }
 
@@ -144,7 +156,6 @@ async function NodeDrawAsync(i) {
         ctx.globalAlpha=nodeAlpha;
         ctx.moveTo(nodeList[i].x, nodeList[i].y);
         ctx.arc(nodeList[i].x, nodeList[i].y, nodeList[i].radius, 0, 6.28318531);
-        ctx.fill();
     }
 }
 //Experimental
@@ -164,14 +175,17 @@ function Renderer() {
         if (nodeList[i].y > maxY || nodeList[i].y < 0) {
             nodeList[i].accelerationY = -nodeList[i].accelerationY;
         }
+        ctx.beginPath();
         nodeList[i].x += nodeList[i].accelerationX;
         nodeList[i].y += nodeList[i].accelerationY;
         //Draw Lines between nearing Nodes
-        ctx.beginPath();
-        LineDrawAsyncExperimental(i);
+        if (enableDrawer) {
+            DrawerAsyncExperimental(i);
+        }
         //Draw Circles were nodes are
         PulseNodes(i);
         NodeDrawAsync(i);
+        ctx.fill();
     }
     requestAnimationFrame(Renderer);
 }
